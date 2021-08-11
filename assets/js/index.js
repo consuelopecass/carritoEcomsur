0db55fa4805382b05a86fdc8478e591d00f2486e
consuelopecass <c.pena.sepulveda@gmail.com>
2021-08-11 17:30:21 -0400

    agregando html de inyectarCards() + conexión con agregarCarrito()

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,4 +13,26 @@ const fetchData = async(busqueda) => {
 const inyectarCards = data => {
     const contenido = document.querySelector('#contenido')
     console.log(data)
+    var html = ''
+    for (var i = 0; i < data.Search.length; i++) {
+        var item = data.Search[i]
+            //peliculas.push(item)
+        html += `<div class="item">
+                        <div class="contenedorPelicula">
+                            <img src="${item.Poster}" alt="${item.Title}">
+                            <h3> Titulo: ${item.Title}</h3>
+                            <p> Año: ${item.Year}</p>
+                            <p> ID: ${item.imdbID}</p>
+                            <p> Tipo: ${item.Type}</p>
+                        </div>
+                        <div class="contenedorBoton">
+                        <button id="comprar" onclick="agregarCarrito('${item.imdbID}')" class="">Comprar</button>
+                        </div>
+                    </div>`
+    }
+    contenido.innerHTML = html
+}
+
+const agregarCarrito = (id) => {
+    alert('me agregaste!')
 }
