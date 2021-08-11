f2cbb1be3a38489f8bfb6bca88eaf621d0e27600
consuelopecass <c.pena.sepulveda@gmail.com>
2021-08-11 17:24:19 -0400

    agregando div busqueda + fetchData() y inyectarCards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,16 @@
-alert('probanding que funciona')
+const fetchData = async(busqueda) => {
+    var busqueda = document.querySelector('#busqueda').value;
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=4213517b&s=${busqueda}`)
+        const data = await res.json();
+        console.log(data)
+        inyectarCards(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const inyectarCards = data => {
+    const contenido = document.querySelector('#contenido')
+    console.log(data)
+}
